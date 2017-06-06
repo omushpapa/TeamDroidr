db029d7ef40991f5fec48a5396e3334f2f478976
Paul Aswa <aswapaul@gmail.com>
2017-06-06 21:15:17 +0300

    Fix: article repetition

--- scraper.py
+++ scraper.py
@@ -15,19 +15,23 @@ def get_highlights(arguments=None):
     soup = BeautifulSoup(page, 'html.parser')
     divs = soup.find_all('div', 'w-blog-post-body')
     highlights_list = []
+    links_list = []
     for i in divs:
         article = i.find('a', 'entry-title')
         try:
             article_link = article.get('href')
         except AttributeError:
             article_link = ''
-        title = article.text
-        date = i.find('time').text
-        timestamp = datetime.strptime(date, "%B %d, %Y").strftime("%s")
-        author = i.find('span', 'author').text
 
-        highlights_list.append((timestamp, encode(title), encode(
-            date), encode(author), encode(article_link)))
+        if article_link not in links_list:
+            links_list.append(article_link)
+            title = article.text
+            date = i.find('time').text
+            timestamp = datetime.strptime(date, "%B %d, %Y").strftime("%s")
+            author = i.find('span', 'author').text
+
+            highlights_list.append((timestamp, encode(title), encode(
+                date), encode(author), encode(article_link)))
 
     if highlights_list:
         highlights = 'Highlights:\n'
