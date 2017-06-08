f1717e4147a8f299d59a18656de68753d99428b7
Paul Aswa <aswapaul@gmail.com>
2017-06-08 23:40:19 +0300

    Add dj_database_url

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 DB_PASS = os.environ['DB_PASS']
 
@@ -16,3 +17,7 @@ INSTALLED_APPS = (
     )
 
 SECRET_KEY = '63cFWu$$lhT3bVP9U1k1Iv@Jo02SuM'
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
